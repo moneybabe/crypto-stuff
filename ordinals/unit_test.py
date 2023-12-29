29f48b989dc4729100a3b842078b9cb7050e8a3b
Neo Lee <neo.lky852@gmail.com>
2023-12-28 19:19:10 -0800

    bug fix

--- ordinals/unit_test.py
+++ ordinals/unit_test.py
@@ -1,5 +1,4 @@
 from classes.Token import Token
-from classes.Tokens import Tokens
 from classes.AddressMonitor import AddressMonitor
 from configparser import ConfigParser
 import pandas as pd
@@ -17,7 +16,7 @@ class TestToken:
     okx_password = config["okx_api"]["password"]
 
     def test_get_token_info(self):
-        ordi = Token("ordi")
+        ordi = Token("ordi", False)
         ordi_data = {
             "tick": "ordi", 
             "inscription_id": "b61b0172d95e266c18aea0c624db987e971a5d6d4ebc2aaed85da4642d635735i0", 
@@ -37,7 +36,7 @@ class TestToken:
             if key != "transactions" and key != "holders":
                 assert getattr(ordi, key) == value
 
-        rats = Token("rats")
+        rats = Token("rats", False)
         rats_data = {
             "tick": "rats", 
             "inscription_id": "77df24c9f1bd1c6a606eb12eeae3e2a2db40774d54b839b5ae11f438353ddf47i0", 
@@ -58,7 +57,7 @@ class TestToken:
                 assert getattr(rats, key) == value
 
     def test_get_floor_listing(self):
-        ordi = Token("ordi")
+        ordi = Token("ordi", False)
         floor_listing = ordi.get_floor_listing(
             self.okx_api_key,
             self.okx_secret_key,
@@ -68,7 +67,7 @@ class TestToken:
         assert len(floor_listing) == 4
         assert ordi.floor_listing == floor_listing
 
-        rats = Token("rats")
+        rats = Token("rats", False)
         floor_listing = rats.get_floor_listing(
             self.okx_api_key,
             self.okx_secret_key,
@@ -79,7 +78,7 @@ class TestToken:
         assert rats.floor_listing == floor_listing
 
     def test_get_trade_history(self):
-        ordi = Token("ordi")
+        ordi = Token("ordi", False)
         trade_history = ordi.get_trade_history(
             self.okx_api_key,
             self.okx_secret_key,
@@ -88,7 +87,7 @@ class TestToken:
         assert type(trade_history) == pd.DataFrame
         assert len(trade_history) > 0
 
-        eight = Token("8888")
+        eight = Token("8888", False)
         trade_history = eight.get_trade_history(
             self.okx_api_key,
             self.okx_secret_key,
@@ -98,7 +97,7 @@ class TestToken:
         assert len(trade_history) > 0 
 
     def test_save_and_load_token(self):
-        ordi = Token("ordi")
+        ordi = Token("ordi", False)
         ordi.get_floor_listing(
             self.okx_api_key,
             self.okx_secret_key,
@@ -115,7 +114,7 @@ class TestToken:
             assert ordi.floor_listing[key] == value
         assert ordi_loaded.trade_history.shape == ordi.trade_history.shape
 
-        eight = Token("8888")
+        eight = Token("8888", False)
         eight.get_floor_listing(
             self.okx_api_key,
             self.okx_secret_key,
@@ -133,58 +132,32 @@ class TestToken:
         assert eight_loaded.trade_history.shape == eight.trade_history.shape
 
     def test_get_top_20_holder(self):
-        ordi = Token("ordi")
+        ordi = Token("ordi", False)
         ordi.get_top_20_holders()
-        assert type(ordi.top_20_holders) == pd.DataFrame
-        assert ordi.top_20_holders.shape == (20, 5)
-        assert ordi.top_20_holders.columns.tolist() == [
+        assert type(ordi.top_20_holders_df) == pd.DataFrame
+        assert ordi.top_20_holders_df.shape == (20, 5)
+        assert ordi.top_20_holders_df.columns.tolist() == [
             "tick",
             "address",
             "overall_balance",
             "transferable_balance",
             "available_balance"
         ]
-        assert ordi.top_20_holders.iloc[-1]["overall_balance"] > 0
+        assert ordi.top_20_holders_df.iloc[-1]["overall_balance"] > 0
         
-        eight = Token("8888")
+        eight = Token("8888", False)
         eight.get_top_20_holders()
-        assert type(eight.top_20_holders) == pd.DataFrame
-        assert eight.top_20_holders.shape == (20, 5)
-        assert eight.top_20_holders.columns.tolist() == [
+        assert type(eight.top_20_holders_df) == pd.DataFrame
+        assert eight.top_20_holders_df.shape == (20, 5)
+        assert eight.top_20_holders_df.columns.tolist() == [
             "tick",
             "address",
             "overall_balance",
             "transferable_balance",
             "available_balance"
         ]
-        assert eight.top_20_holders.iloc[-1]["overall_balance"] > 0
+        assert eight.top_20_holders_df.iloc[-1]["overall_balance"] > 0
 
-class TestTokens:
-
-    def test_initialize(self):
-        tick_list = ["ordi", "8888"]
-        tokens = Tokens(tick_list)
-        assert type(tokens.tokens) == dict
-        assert len(tokens.tokens) == 2
-        for token in tokens.tokens.values():
-            assert type(token) == Token
-            assert token.tick in ["ordi", "8888"]
-
-    def test_add_token(self):
-        tick_list = ["ordi", "8888"]
-        tokens = Tokens(tick_list)
-        assert len(tokens.tokens) == 2
-        tokens.add_token("rats")
-        assert len(tokens.tokens) == 3
-
-    def test_save_tokens(self):
-        tick_list = ["ordi", "8888"]
-        tokens = Tokens(tick_list)
-        tokens.save_tokens()
-        tokens_loaded = Tokens(tick_list, load=True)
-        for tick, token in tokens_loaded.tokens.items():
-            assert tokens.tokens[tick].holders == token.holders
-            assert tokens.tokens[tick].transactions == token.transactions
 
 class TestAddressMonitor:
 
